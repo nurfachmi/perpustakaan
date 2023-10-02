84b1d016f9037057b00a6ed39765e3fc217b0f6d
“Mohammad <firmansmoh842@gmail.com>
2023-10-02 16:14:12 +0700

    delete array_push

--- app/Http/Controllers/Datatables/BookDatatables.php
+++ app/Http/Controllers/Datatables/BookDatatables.php
@@ -18,24 +18,13 @@ class BookDatatables extends Controller
         return DataTables::of($query)
         ->addColumn('action', function ($row) {
                 $data = [
+                    'edit_url'     => route('books.edit', ['book' => $row->getKey()]),
+                    'delete_url'   => route('books.destroy', ['book' => $row->getKey()]),
                     'redirect_url' => route('books.index'),
                     'name'         => $row->title,
                     'resource'     => 'books',
                     'custom_links' => []
                 ];
-
-                array_push($data['custom_links'],
-                    [
-                     'label' => 'Edit',
-                     'url' => route('books.edit', ['book' => $row->getKey()]),
-                     'books' => 'books.index'
-                    ],
-                    [
-                     'label' => 'Delete',
-                     'url' => route('books.destroy', ['book' => $row->getKey()]),
-                     'books' => 'books.destroy'
-                    ]
-                );
 
                 return view('components.datatable-action', $data);
             })
