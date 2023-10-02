6538d6c0b9d2c2700657702f31c76c8865b454fe
AaEzha <aaezha@gmail.com>
2023-10-03 03:38:37 +0700

    migrations 🔥

---
+++ database/migrations/2023_10_02_200305_create_borrows_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('borrows', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->date('start_at');
+            $table->date('end_at');
+            $table->date('return_at')->nullable();
+            $table->unsignedInteger('books_borrowed')->default(0);
+            $table->unsignedInteger('books_returned')->default(0);
+            $table->unsignedDouble('late_price')->default(config('perpustakaan.late_price'));
+            $table->unsignedDouble('book_price')->default(config('perpustakaan.book_price'));
+            $table->string('created_by')->nullable()->default('System');
+            $table->string('updated_by')->nullable()->default('System');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('borrows');
+    }
+};
---
+++ database/migrations/2023_10_02_201108_create_borrow_books_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Borrow;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('borrow_books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Borrow::class)->constrained();
+            $table->string('isbn');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('borrow_books');
+    }
+};
