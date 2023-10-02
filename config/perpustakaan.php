6dcd841398db19a19e9fd1235fefe58feba5722c
AaEzha <aaezha@gmail.com>
2023-10-03 03:39:11 +0700

    config 🔥

---
+++ config/perpustakaan.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /**
+     * Lamanya peminjaman.
+     */
+    'borrow_days' => 3,
+
+    /**
+     * Besar denda keterlambatan per hari.
+     */
+    'late_price' => 500,
+
+    /**
+     * Besar denda kehilangan per buku.
+     */
+    'book_price' => 50000,
+];
