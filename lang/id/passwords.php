a5de7db281f1f20e567c8b1cd0773faf2d2a5a4f
AaEzha <rznrfchm@gmail.com>
2023-10-07 09:18:56 +0700

    lang: id default 🇮🇩

---
+++ lang/id/passwords.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Password Anda telah dipulihkan!',
+    'sent' => 'Kami telah mengirimkan email tentang tautan pemulihan password Anda!',
+    'throttled' => 'Mohon tunggu sebelum mencoba kembali.',
+    'token' => 'Token pemulihan password ini tidak valid.',
+    'user' => "Sistem kami tidak mendapati akun dengan alamat surel tersebut.",
+    'wrong' => "Password lama Anda kurang tepat.",
+    'same' => "Password baru tidak boleh sama dengan password sebelumnya.",
+
+];
